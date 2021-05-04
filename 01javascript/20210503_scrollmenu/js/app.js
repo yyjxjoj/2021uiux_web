c901c60504aef7e30fd042c255ae95b0719db31a
yyjxjoj <insane0114@gmail.com>
2021-05-04 10:21:44 +0900

    1

--- 01javascript/20210503_scrollmenu/js/app.js
+++ 01javascript/20210503_scrollmenu/js/app.js
@@ -39,10 +39,37 @@ $(document).ready(function(){
     $("html,body").animate({scrollTop:st})
   });
 
+$(window).scroll(function(){
+  let scrollY = $(window).scrollTop();
+
+  // if(scrollY >= $(".scene").eq(0).offset().top){
+  //   $(".menu>a").removeClass("active").eq(0).addClass("active");
+  // }
+  // if(scrollY >= $(".scene").eq(1).offset().top){
+  //   $(".menu>a").removeClass("active").eq(1).addClass("active");
+  // }
+  // if(scrollY >= $(".scene").eq(2).offset().top){
+  //   $(".menu>a").removeClass("active").eq(2).addClass("active");
+  // }
+  //
+  // for(let i=0,i<$(".scene").length;i++){
+  //   if(scrollY >= $(".scene").eq(i).offset().top){
+  //     $(".menu>a").removeClass("active").eq(i).addClass("active");
+  //   }
+  // }
+
+  $(".scene").each(function(index){
+  //   if(scrollY >= $(".scene").eq(i).offset().top){
+    if(scrollY >= $(this).offset().top){
+      $(".menu>a").removeClass("active").eq(index).addClass("active");
+    }
+  });
+
+
+}); //scroll
 
 
 
 
 
-
-});
+}); //jquery ready
