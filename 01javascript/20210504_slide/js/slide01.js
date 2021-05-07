0818fc04ce8d328845f87c3d95f454292ccc8b28
yyjxjoj <insane0114@gmail.com>
2021-05-07 14:39:10 +0900

    1

--- 01javascript/20210504_slide/js/slide01.js
+++ 01javascript/20210504_slide/js/slide01.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
   // $(".wrapper li").eq(0).animate({left:"-100%"},1000);
   // $(".wrapper li").eq(1).animate({left:0},1000);
+
   let current = 0;
   const banner = $(".wrapper li");
-  console.log(current);
-  function slider(){ // 0 1 2 3 4
+
+  function slider(){
     banner.eq(current).css("left",0).stop().animate({left:"-100%"},1000);
     current++;
-    if(current == 5){current=0}
-    banner.eq(current).css("left","100%").stop().animate({left:0},1000);
+    if(current == 5 ){current=0}
+    banner.eq(current).css({left:"100%"}).stop().animate({left:0},1000);
   }
-
   setInterval(slider,3000);
+
+
 });
